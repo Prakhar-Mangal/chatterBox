ba4652d7d96f23acc9474c9f1f272314c81efcdf
Prakhar-Mangal <41920289+Prakhar-Mangal@users.noreply.github.com>
2019-01-06 22:29:37 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,41 @@
+var database;
+function fun(){
+	var nam=document.getElementById("text1").value;
+	var usr=document.getElementById("text2").value;
+	 database=firebase.database();
+  var ref=database.ref('chat');
+  var data={
+  	name:nam,
+  	msg: usr
+  }
+  ref.push(data);
+  
+  ref.on('value',gotData,errData);
+  function gotData(data){
+  	//console.log(data.val());
+  	var chat=data.val();
+  	var keys=Object.keys(chat);
+  	var list = document.getElementById('chatlist');
+  	$( "#chatlist" ).empty();
+  	//console.log(keys);
+  	for(i=0;i<keys.length;i++){
+  		var k=keys[i];
+  		var nam=chat[k].name;
+  		var msg=chat[k].msg;
+  
+  		//console.log(nam,msg);
+  		//var list = document.getElementById('chatlist');
+  		var entry = document.createElement('li');
+  		entry.appendChild(document.createTextNode(nam+': '+msg));
+  		list.appendChild(entry);
+  		
+  	}
+  }
+  function errData(err){
+  	console.log("Error");
+  	console.log(err);
+
+  }
+	//console.log(firebase);
+	
+}
